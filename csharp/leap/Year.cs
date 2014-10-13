cc8a4a9246282a795bfc6490ab9c298f9cf20f3d
jraguilo <johnreyaguilo@gmail.com>
2014-10-13 12:04:13 -0700

    Adding completed first assignments for python and csharp

---
+++ csharp/leap/Year.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Year
+{
+    public static Boolean IsLeap(int year)
+    {
+        if( year % 4 == 0)
+        {
+            if (year % 100 == 0 && year % 400 != 0)
+                return false;
+            return true;
+        }
+        return false;
+    }
+}
