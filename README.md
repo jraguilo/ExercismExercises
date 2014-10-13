bb62e22d673b29164577a900ebfa5793b11176b2
jraguilo <johnreyaguilo@gmail.com>
2014-10-13 14:08:49 -0700

    Added ETL exercise and updated readme to keep track of assignment numbers

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ ExercismExercises
 =================
 
 Programming assignments completed for http://exercism.io/
+
+
+Completed Exercises In Order Assigned:
+
+C#:
+1. Leap
+2. Bob
+3. ETL
+
+Python:
+1. Bob
+2. Leap
---
+++ csharp/etl/ETL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ETL
+{
+    public static Dictionary<string, int> Transform(Dictionary<int, IList<string>> oldScores)
+    {
+        Dictionary<string, int> newScores = new Dictionary<string, int>();
+
+        foreach(KeyValuePair<int, IList<string>> score in oldScores)
+        {
+            int points = score.Key;
+            foreach(string item in score.Value)
+            {
+                newScores.Add(item.ToLower(), points);
+            }
+        }
+
+        return newScores;
+    }
+}
