9edbe48a7e9d82e370ed25ff8beb9b3f2b16c4d3
jraguilo <johnreyaguilo@gmail.com>
2014-10-13 12:44:04 -0700

    Added csharp bob exercise

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+public class Bob
+{
+    public string Hey(string message)
+    {
+        //No message
+        if(string.IsNullOrWhiteSpace(message))
+        {
+            return "Fine. Be that way!";
+        }
+
+        //Message is a yell
+        if(message.Any(c => char.IsLetter(c)) && message.ToUpper() == message)
+        {
+            return "Whoa, chill out!";
+        }
+
+        //Message is a question
+        if(message.EndsWith("?"))
+        {
+            return "Sure.";
+        }
+
+        //All other messages
+        return "Whatever.";
+    }
+
+}
