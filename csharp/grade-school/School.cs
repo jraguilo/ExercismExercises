a5474bb9a638ee4128424c2d5a37bc3e072e2d54
jraguilo <johnreyaguilo@gmail.com>
2014-10-16 10:43:34 -0700

    Added grade school exercise.

---
+++ csharp/grade-school/School.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class School
+{
+    public Dictionary<int, List<string>> Roster;
+
+    public School()
+    {
+        Roster = new Dictionary<int, List<string>>();
+    }
+
+    public void Add(string name, int grade)
+    {
+        if(Roster.ContainsKey(grade))
+            Roster[grade].Add(name);
+        else
+        {
+            Roster.Add(grade, new List<string> {name});
+        }
+        Roster[grade].Sort();
+    }
+
+    public List<string> Grade(int grade)
+    {
+        if (Roster.ContainsKey(grade))
+            return Roster[grade];
+        return new List<string>();
+    }
+}
