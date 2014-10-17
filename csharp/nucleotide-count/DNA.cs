febe66d63c868727e45cd882b70d19e1456a33ec
jraguilo <johnreyaguilo@gmail.com>
2014-10-17 10:03:06 -0700

    Added Nucleotide exercise

---
+++ csharp/nucleotide-count/DNA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DNA
+{
+    public Dictionary<char, int> NucleotideCounts;
+
+    public DNA(string strand = "")
+    {
+        NucleotideCounts = new Dictionary<char, int> { { 'A', 0 }, { 'T', 0 }, { 'C', 0 }, { 'G', 0 } };
+        strand = strand.Trim();
+        if (strand != "")
+        {
+            foreach(char nucleotide in strand)
+            {
+                NucleotideCounts[nucleotide]++;
+            }
+        }
+    }
+
+
+    public int Count(char nucleotide)
+    {
+        if (nucleotide != 'A' && nucleotide != 'T' && nucleotide != 'C' && nucleotide != 'G')
+            throw new InvalidNucleotideException();
+        return NucleotideCounts[nucleotide];
+    }
+}
+
+public class InvalidNucleotideException : Exception { }
